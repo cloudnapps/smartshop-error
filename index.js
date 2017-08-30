b681131c02b9b06168c09c0403dc56fa64f6f56b
GongXian Cao <gongxiancao@hotmail.com>
2017-08-30 17:34:50 +0800

    add error codes

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  AlreadyActivatedError:                        'already activated',
+  ClientNotExistError:                          'client not exist',
+  CubePointAggregationFailedError:              'failed to aggregate cube point',
+  CubePointNotExistError:                       'cube point not exist',
+  Success:                                      'success',
+  SystemBusyError:                              'system busy, retry later',
+  TokenExpiredError:                            'token is expired',
+  UserAlreadySignupError:                       'failed to signup, user already signup',
+  UserNotExistError:                            'user not exist',
+  WrongParameterError:                          'wrong parameter',
+  InternalError:                                'internal error',
+  ResourceNotFoundError:                        'resource not found'
+};
